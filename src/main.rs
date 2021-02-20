7ce6a98169495fdd6f9d2df68d29a0041a94fe07
Simon Schneemelcher <s.schneemelcher@tu-bs.de>
2021-02-20 19:14:11 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::net::UdpSocket;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::io::{Write, BufRead, stdout, stdin};
+use std::io::ErrorKind;
+use std::time::Duration;
+
+
+fn main() {
+	let socket = UdpSocket::bind("127.0.0.1:8001").expect("couldn't bind to address");	
+	socket.set_nonblocking(true).expect("could not set nonblocking");
+	let mut buf = [0u8; 1024];
+	let msg_mutex = Arc::new(Mutex::new(String::new()));	
+	let arc_mutex = Arc::clone(&msg_mutex);
+
+	// spawning a thread to handle the user input because its blocking
+	thread::spawn(move ||{
+		let stdin = stdin();
+		for line in stdin.lock().lines() {	
+			*arc_mutex.lock().unwrap() = String::from(line.unwrap());
+		}
+	});
+
+	loop { // check every 50 ms if there is a new message
+	        if *msg_mutex.lock().unwrap() != ""{ // checks the mutex for new user input to send
+			let mut msg = msg_mutex.lock().unwrap();
+			socket.send_to((*msg).as_bytes(), "127.0.0.1:8000").ok();
+			println!("You wrote: {}\n", *msg);
+			*msg = "".to_string();
+		}
+		let result = socket.recv_from(&mut buf);
+		match result {
+			Ok((n, addr)) => {
+				print!("{:?} wrote: ", addr);
+				stdout().write(&mut buf[..n]).ok();
+				stdout().flush().ok();
+				print!("\n\n");
+			},
+			Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
+				println!("received error: {}", err)
+			}
+			_ => {}
+		}			
+		thread::sleep(Duration::from_millis(50));
+	}
+}
